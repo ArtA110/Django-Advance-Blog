66eaeda1d2218a032d9620bd3a0122fe2bb629ba
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-03-17 10:31:43 +0330

    Use Generic API views in blog API

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,5 +4,5 @@ from blog.api.v1 import views
 app_name = 'api-v1'
 urlpatterns = [
     path('posts/', views.PostList.as_view(), name='post-list'),
-    path('post/<int:id>/', views.PostDetail.as_view(), name='post-detail')
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post-detail')
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -6,8 +6,10 @@ from blog.models import Post
 from django.shortcuts import get_object_or_404, get_list_or_404
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 
 
+'''
 class PostList(APIView):
     """show and add posts"""
     permission_classes = [IsAuthenticated]
@@ -17,16 +19,10 @@ class PostList(APIView):
         posts = get_list_or_404(Post, status=1)
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+'''
 
-    def post(self, request):
-        """add a post"""
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-
-class PostDetail(APIView):
+'''
+class PostDetail(GenericAPIView):
     """ Show a Single post object plus editing and removing it """
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
@@ -50,4 +46,17 @@ class PostDetail(APIView):
         post = get_object_or_404(Post, pk=id, status=1)
         post.delete()
         return Response({"detail": "post deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+'''
 
+class PostList(ListCreateAPIView):
+    """show and add posts"""
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    queryset = get_list_or_404(Post, status=1)
+
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    """ Show a Single post object plus editing and removing it """
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=1)
