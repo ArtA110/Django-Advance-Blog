574f544c6cf4c46f305e681cdc38f949689c7e08
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-03-18 20:11:08 +0330

    Update API Documentation + Add download json path

--- core/core/urls.py
+++ core/core/urls.py
@@ -24,12 +24,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Blog API",
       default_version='v1',
-      description="Test description",
+      description="This is a blog project for fun!",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      contact=openapi.Contact(email="ali.tabatabaeian16@gmail.com"),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
@@ -40,7 +40,7 @@ urlpatterns = [
     path('blog/', include(urls)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
