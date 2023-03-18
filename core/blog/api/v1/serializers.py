fd63419efc4a1e22945e5e0035a6c29ef368c9da
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-03-18 17:09:46 +0330

    add category field to API + API fields now make more sense

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -2,20 +2,36 @@ from rest_framework import serializers
 from blog.models import Post, Category
 
 
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+
+
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
     absolute_url = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ['id', 'title', 'status', 'content', 'snippet', 'author', 'absolute_url', 'relative_url',
+        fields = ['id', 'title', 'status', 'content', 'snippet', 'category', 'author', 'absolute_url', 'relative_url',
                   'created_date', 'published_date']
+
     def get_absolute_url(self, obj):
             request = self.context.get('request')
             return request.build_absolute_uri(obj.pk)
 
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop('snippet', None)
+            rep.pop('absolute_url', None)
+            rep.pop('relative_url', None)
+        else:
+            rep.pop('content', None)
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
+        rep['category'] = CategorySerializer(instance.category).data
+        return rep
