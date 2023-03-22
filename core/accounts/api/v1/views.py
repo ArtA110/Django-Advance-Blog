66dcfa37550caf917f3a4d717cff219f69f2ecb0
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-03-22 17:02:39 +0430

    Initial User API

---
+++ core/accounts/api/v1/views.py
@@ -0,0 +1,17 @@
+from rest_framework.generics import GenericAPIView
+from . import serializers
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class RegistrationApiView(GenericAPIView):
+    serializer_class = serializers.RegistrationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                'email': serializer.validated_data['email']
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
