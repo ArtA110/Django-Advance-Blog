ad87840cee351f4178c6c6623ef737f40e2c1ce9
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-03-25 20:18:29 +0430

    Make accounts app folder more organized

--- core/accounts/api/v1/urls/accounts.py
+++ core/accounts/api/v1/urls/accounts.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from . import views
+from .. import views
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
 
 
-app_name = 'api-v1'
 urlpatterns = [
     path('regitration/', views.RegistrationApiView.as_view(), name='registration'),
     path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
@@ -15,6 +14,4 @@ urlpatterns = [
     path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
     path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
     path('change-password/', views.ChangePasswordApiView.as_view(), name='change-password'),
-    path('profile/', views.ProfileApiView.as_view(), name='profile'),
-
 ]
