4f3566ae7b1715667f4c4fb90ac3c6c667a41848
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-03-31 18:03:10 +0430

    Add fixtures + Some tests

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -2,17 +2,29 @@ import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
 from datetime import datetime
+from accounts.models import User
+
+
+@pytest.fixture()
+def api_client():
+    client = APIClient()
+    return client
+
+
+@pytest.fixture()
+def common_user():
+    user = User.objects.create_user(email='test@test.ts', password='A@/123456', is_verified = True)
+    return user
 
 
 @pytest.mark.django_db
 class TestPostApi:
-    client = APIClient()
-    def test_get_post_response_200(self):
+    def test_get_post_response_200(self, api_client):
         url = reverse('blog:api-v1:post-list')
-        response = self.client.get(url)
+        response = api_client.get(url)
         assert response.status_code == 200
 
-    def test_create_post_response_401(self):
+    def test_create_post_response_401(self, api_client):
         url = reverse('blog:api-v1:post-list')
         data = {
             'status': True,
@@ -20,5 +32,29 @@ class TestPostApi:
             'title': 'test',
             'published_date': datetime.now()
         }
-        response = self.client.post(url, data=data)
+        response = api_client.post(url, data=data)
         assert response.status_code == 401
+
+    def test_create_post_response_201(self, api_client, common_user):
+        url = reverse('blog:api-v1:post-list')
+        data = {
+            'status': True,
+            'content': 'test',
+            'title': 'test',
+            'published_date': datetime.now()
+        }
+        user = common_user
+        api_client.force_login(user=user)
+        response = api_client.post(url, data=data)
+        assert response.status_code == 201
+
+    def test_create_post_bad_data_response_201(self, api_client, common_user):
+        url = reverse('blog:api-v1:post-list')
+        data = {
+            'status': True,
+            'content': 'test',
+        }
+        user = common_user
+        api_client.force_login(user=user)
+        response = api_client.post(url, data=data)
+        assert response.status_code == 400
