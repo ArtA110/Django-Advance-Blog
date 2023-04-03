f27cf87f1bd631beeba49a766fb45577d765fa1c
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-04-03 08:40:49 +0430

    Add Redis + celery

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-# from accounts import views
+from accounts import views
 
 app_name = "accounts"
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("send-email/", views.send_email),
+    path("test/", views.test),
     path("api/v1/", include("accounts.api.v1.urls")),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
